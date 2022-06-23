d9a7ed77550f0870cf20af9ad4c95fe8f9e346fb
beastvold <31387941+beastvold@users.noreply.github.com>
2022-06-23 16:23:26 -0700

    Adding flex projects page

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-let bgImageArray = [
+const bgImageArray = [
     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/full-lonely.jpg",
     "https://images.unsplash.com/photo-1468413253725-0d5181091126?w=2000",
     "https://images.unsplash.com/photo-1531366936337-7c912a4589a7?w=2000",
@@ -6,11 +6,11 @@ let bgImageArray = [
 ]
 
 let imgIndex = 0;
-let carousel = document.querySelector(".carousel");
+const carousel = document.querySelector(".carousel");
 carousel.style.backgroundImage = `url(${bgImageArray[imgIndex]})`;
 
 // Event listener for our next button
-let carouselNextBtn = document.querySelector(".btn-next");
+const carouselNextBtn = document.querySelector(".btn-next");
 carouselNextBtn.addEventListener("click", function () {
     // If we are at the last image, rotate through to the first
     if (imgIndex === bgImageArray.length - 1) {
@@ -26,7 +26,7 @@ carouselNextBtn.addEventListener("click", function () {
 });
 
 // Event listener for our previous button
-let carouselPrevBtn = document.querySelector(".btn-prev");
+const carouselPrevBtn = document.querySelector(".btn-prev");
 carouselPrevBtn.addEventListener("click", function () {
     // If we are at the first image, rotate through to the last
     if (imgIndex === 0) {
